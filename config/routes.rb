8a1d670c9af272ae85d1f6c6085d378d5a594c42
priti kantesaria <priti.kantesaria@kellogg.northwestern.edu>
2025-05-20 05:04:40 +0000

    final edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   
-  resources "places"
+  get("/", { :controller => "places", :action => "index" })
+
   get("/places", {:controller => "places", :action => "index"})
   get("/places/new", {:controller => "places", :action => "new"})
   post("/places", {:controller => "places", :action => "create"})
   get("/places/:id", {:controller => "places", :action => "show"})
 
-  resources "entries"
-  # get("/entries", {:controller => "entries", :action => "index"})
   get("/entries/new", {:controller => "entries", :action => "new"})
-  post("/entries", {:controller => "places", :action => "create"})
+  post("/entries", {:controller => "entries", :action => "create"})
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Place.destroy_all
+Entry.destroy_all
+
+Place.create(name: "Mexico City")
+Place.create(name: "Charleston")
+Place.create(name: "Beijing")
+Place.create(name: "Amsterdam")
+
+Entry.create(title: "Ate tacos", description: "something, something, something...", occurred_on: "2022-01-01", place_id: "1")
+Entry.create(title: "Lisa and Jim's wedding", description: "something, something, something...", occurred_on: "2022-01-01", place_id: "1")
+Entry.create(title: "Ate tacos again", description: "something, something, something...", occurred_on: "2022-01-01", place_id: "1")
